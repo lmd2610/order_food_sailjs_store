37ef5d3ada54d23d45cedd716ed4184f4d900460
Duc La <duclm@media-one.vn>
2020-12-15 02:52:39 +0700

    init commit

--- source/config/policies.js
+++ source/config/policies.js
@@ -22,36 +22,12 @@ module.exports.policies = {
   '*': true,
   'swagger': true,
   'swagger/swagger.json': true,
-  'user/get-all': 'customer',
-  'user/login': 'notAuthCustomer',
-  'user/register': 'notAuthCustomer',
-  'user/get-one': 'customer',
-  'user/update': 'customer',
-  'user/cancel-order':'customer',
-
-
-  'category/list-category': 'customer',
-
-  'order/calculate-bill': 'customer',
-  'order/create': 'customer',
-  'order/list-order-by-store': 'store',
+ 
 
   'store/create': 'notAuthStore',
-  'store/get-all': 'customer',
-  'store/get-store-by-id': 'customer',
+  'store/get-all': 'store',
+  'store/get-store-by-id': 'store',
   'store/login': 'notAuthStore',
 
-  'food/search-food': 'customer',
-  'food/get-food-by-storeid': 'customer',
-
-
-  'menu/get-menu-by-storeid': 'customer',
-
-  'shipper/create':'notAuthShipper',
-  'shipper/list-order':'shipper',
-  'shipper/login': 'notAuthShipper',
-  'shipper/accept-order': 'shipper',
-  'shipper/cancel-order': 'shipper',
-  'shipper/received-food': 'shipper',
-  'shipper/shipped-food': 'shipper',
+  
 };
